66b4b8673ee29c3a59106ac4ce96f4c08ddbd290
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-12 11:52:29 +0530

    I added variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1 = Ramesh
-PERSON2 = Suresh
+PERSON1=Ramesh
+PERSON2=Suresh
 
 echo "$PERSON1:: Hi ${PERSON2}, How are u?"
 echo "${PERSON2}:: Hello $PERSON1. I'm fine, How r u?"
