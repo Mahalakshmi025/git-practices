33d7ef998e0b47de7d2375ba246d10de5e9d8594
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-12 13:01:46 +0530

    arrays

--- 11-installation.sh
+++ 11-installation.sh
@@ -7,7 +7,30 @@
 USERID=$(id -u)
 echo "$USERID"
 
-# if [ $USERID -ne 0 ]
-# then
-#    echo "please run this script with root priveleges"
-#    exit 1
+#OUTPUT: sh 11-installation.sh
+#        1001
+
+if [ $USERID -ne 0 ]
+then
+   echo "please run this script with root priveleges"
+   exit 1
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+   echo "git is not installed, going to install it..."
+   dnf install git -y
+
+   if [ $? -ne 0 ]
+   then 
+      echo "git installation is not success, check is"
+      exit 
+   else 
+      echo "Git installation is success"
+    fi
+    
+else
+   echo " git is already installed, nothing to do"
+fi
