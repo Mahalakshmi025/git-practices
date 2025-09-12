aa8bb20bbe6dee15c506d9335798d852c7f08413
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-12 11:38:40 +0530

    I added echo conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hi Suresh, How are u?"
+echo "Suresh: Hello Ramesh. I'm fine, How r u?"
+echo "Ramesh: I'm doing good suresh. What's going on?"
+echo "Suresh: I started learning shell script Ramesh"
