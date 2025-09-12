c2602f166d0a281c305f1d809667a63c93c559f1
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-12 12:47:29 +0530

    arrays

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -2,7 +2,7 @@
 
 NUMBER=$1
 
-if [ $NUMBER -gt 20 ]
+if [ $NUMBER -gt 20 ] # gt -> greaterthan, lt -> lessthan, eq -> equal, -ne -> notequal
 then
 
   echo "Given number: $NUMBER is >20"
@@ -10,5 +10,8 @@ then
 else
 
   echo "Given number: $NUMBER is <20"
-  
+
 fi
+
+# RESULT: sh 10-conditions.sh 19
+#         Given number: 19 is <20
