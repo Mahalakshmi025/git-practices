5f009cf01d15a1cc8b0aa055093fab5e0184de88
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-12 15:04:33 +0530

    arrays

--- 13-colors.sh
+++ 13-colors.sh
@@ -3,6 +3,7 @@
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
+Y="\e[33m"
 N="\e[0m"
 
 CHECK_ROOT() {
@@ -16,9 +17,9 @@ CHECK_ROOT() {
 VALIDATE() {
     if [ $1 -ne 0 ]
     then
-       echo "$2 is ....$R FAILED $N"
+       echo -e "$2 is ....$R FAILED $N"
     else 
-       echo "$2 is ....$G SUCCESS $N"
+       echo -e "$2 is ....$G SUCCESS $N"
     fi
 }
 
@@ -32,7 +33,7 @@ then
    dnf install git -y
    VALIDATE $? "INSTALING GIT"
 else
-   echo "Git is already installed..Nothing to do ..."
+   echo -e "$Y Git is already installed..Nothing to do ..."
 fi
 
 dnf list installed mysql
@@ -43,6 +44,6 @@ then
    dnf install mysql -y
    VALIDATE $? "INSTALING MySQL"
 else
-   echo "Mysql is already installed..Nothing to do ..."
+   echo -e "$Y Mysql is already installed..Nothing to do ..."
 fi
 
