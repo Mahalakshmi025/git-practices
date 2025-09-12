e182a65797a6dd5f206c4160c4d20bfd7f52d391
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-12 12:48:58 +0530

    arrays

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -13,5 +13,13 @@ else
 
 fi
 
-# RESULT: sh 10-conditions.sh 19
+# RESULT: 
+# CASE-1: sh 10-conditions.sh 19
 #         Given number: 19 is <20
+
+# CASE-2: sh 10-conditions.sh 20
+#         Given number: 20 is <20
+
+# CASE-3: sh 10-conditions.sh 21
+#         Given number: 20 is >20
+
