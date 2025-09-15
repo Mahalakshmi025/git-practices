acbeec256825758758a3a129149c152ac0018c6c
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-15 16:13:29 +0530

    changes

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -e #setting the automatic exit , if weget error
+#set -e #setting the automatic exit , if weget error
+set -ex
 echo "Hello world"
 echooo "hello world failure"
 echo "heloo world failure"
