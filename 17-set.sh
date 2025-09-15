f46ad572a6a46869af1f087a6b16e4879eadd705
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-15 16:18:54 +0530

    changes

--- 17-set.sh
+++ 17-set.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-#set -e #setting the automatic exit , if weget error
-set -ex
+set -e #setting the automatic exit , if weget error
+#set -ex #debug mode
+
+failure(){
+
+     echo "Failed at: $1:$2"
+}
+
+trap 'failure "${LINENO}" "$BASH_COMMAND" ' ERR #ERR is the error signal
 echo "Hello world"
 echooo "hello world failure"
 echo "heloo world failure"
