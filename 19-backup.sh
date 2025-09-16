832f26804ee021b4a4988b890d7ad8883d0467e2
Mahalakshmi025 <pmahalakshmi025@gmail.com>
2025-09-16 14:33:16 +0530

    create backup file

--- 19-backup.sh
+++ 19-backup.sh
@@ -24,20 +24,29 @@ then
 #    exit 1
  fi
 
-# if [ ! -d $SOURCE_DIR ]
-# then
-#    echo "$SOURCE_DIR does not exist...please check"
-# fi
+if [ ! -d $SOURCE_DIR ]
+then
+   echo "$SOURCE_DIR does not exist...please check"
+fi
 
-# if [ ! -d $DEST_DIR ]
-# then
-#    echo "$DEST_DIR does not exist...please check"
-# fi
+if [ ! -d $DEST_DIR ]
+then
+   echo "$DEST_DIR does not exist...please check"
+fi
 
-# FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
-# echo "Files: $FILES"
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
+#echo "Files: $FILES"
 
 # if [ ! -z $FILES ] #true if FILES is empty, ! makes it expression false
 # then
-#    echo "Files are found"
-#    ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+#      echo "Files are found"
+#      ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
+#      find $(SOURCE_DIR) -name
+
+if [ -n $FILES ]
+then
+   echo "files r found"
+else
+   echo "no files are older thay $DAYS"
+fi
+
